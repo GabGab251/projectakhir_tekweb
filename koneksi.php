5bfc915d35a7cd0a89bf5c531c7022e1fd69c190
GabGab251 <c14240170@john.petra.ac.id>
2024-12-06 00:03:23 +0700

    perubahan koneksi

--- koneksi.php
+++ koneksi.php
@@ -8,9 +8,9 @@ $database = 'projek_tekweb';
 // Membuat koneksi
 $conn = mysqli_connect($host, $user, $password, $database);
 
-// Cek koneksi
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
-echo "Koneksi berhasil!";
+// // Cek koneksi
+// if (!$conn) {
+//     die("Koneksi gagal: " . mysqli_connect_error());
+// }
+// echo "Koneksi berhasil!";
 ?>
